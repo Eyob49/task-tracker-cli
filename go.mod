70324bba48e902ca28a85e776d97a172e04f6cb3
Eyob <eyobk740@gmail.com>
2026-06-26 20:49:03 +0300

    init commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Eyob49/task-tracker-cli
+
+go 1.26.1
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func Hello() string {
+	return "Hello, World!"
+}
+
+func main() {
+	res := "hahaha"
+	fmt.Println(res)
+	fmt.Println(Hello())
+}
