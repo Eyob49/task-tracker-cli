d3a2cdac73e4842b9117378dc3a989f99a4ba74e
Eyob <eyobk740@gmail.com>
2026-06-27 12:39:57 +0300

    Define task struct with JSON tags

--- task.go
+++ task.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"time"
+)
+
+type Task struct {
+	ID          int       `json:"id"`
+	Description string    `json:"description"`
+	Status      string    `json:"status"`
+	CreatedAt   time.Time `json:"createdAt"`
+	UpdatedAt   time.Time `json:"updatedAt"`
+}
+
+func add(newTask string) string {
+	tasks := Load()
+	taskID := 0
+	if len(tasks) == 0 {
+		taskID = 1
+	} else {
+		taskID = tasks[len(tasks)-1].ID + 1
+	}
+}
